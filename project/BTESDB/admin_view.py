1d25364ce147509301974764a97503227d125959
DianaRenyao <857644432@qq.com>
2018-10-10 16:06:38 +0800

    更改了pdf位置，加入了加密；添加了管理员功能的一些函数

--- project/BTESDB/admin_view.py
+++ project/BTESDB/admin_view.py
@@ -44,6 +44,8 @@ def show_users_all(request):
         response['error_num'] = 1
     return JsonResponse(response)
 
+
+
 def show_user_projects(request):
     response = {}
     try:
@@ -59,4 +61,65 @@ def show_user_projects(request):
         print (str(e))
         response['msg'] = str(e)
         response['error_num'] = 1
+    return JsonResponse(response)
+
+def allow_user(request):
+    response = {}
+    try:
+        username=request.GET['username']
+        this_user=User_Info.objects.get(username=username)
+        this_user.is_staff=True
+        response['msg']='成功通过用户申请'
+        response['error_num']=0
+    except Exception as e:
+        print (str(e))
+        response['msg'] = str(e)
+        response['error_num']=1
+    return JsonResponse(response)
+
+def ban_user(request):
+    response = {}
+    try:
+        username=request.GET['username']
+        this_user=User_Info.objects.get(username=username)
+        this_user.is_staff=False
+        response['msg']='成功禁用用户'
+        response['error_num']=0
+    except Exception as e:
+        print (str(e))
+        response['msg'] = str(e)
+        response['error_num']=1
+    return JsonResponse(response)
+
+def filter_user(request):
+    response={}
+    try:
+        is_staff=request.GET['is_staff']
+        users=User_Info.objects.filter(is_staff=is_staff)
+        response['users']=json.loads(serializers.serialize("json", users))
+        response['msg']='success'
+        response['error_num']=0
+    except Exception as e:
+        print (str(e))
+        response['msg']=str(e)
+        response['error_num']=1
+    return JsonResponse(response)
+
+from itertools import chain
+def search_user(request):
+    response={}
+    try:
+        search_condition=request.GET['search_condition']
+        users1=User_Info.objects.filter(truename__contains=search_condition)
+        users2=User_Info.objects.filter(telephone__contains=search_condition)
+        users3=User_Info.objects.filter(architect_id__contains=search_condition)
+        #可能会出现重复的元素
+        users=chain(users1,users2,users3)
+        response['users']=json.loads(serializers.serialize("json", users))
+        response['msg']='success'
+        response['error_num']=0
+    except Exception as e:
+        print (str(e))
+        response['msg']=str(e)
+        response['error_num']=1
     return JsonResponse(response)
